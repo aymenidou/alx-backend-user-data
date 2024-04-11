38f142ec921afd5889703dc3afd834a943255d0a
Aymen Idouahman <aymanidou@gmail.com>
2024-04-11 21:37:20 +0000

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> str:
     '''function that expects one string argument name password and returns
       a salted, hashed password, which is a byte string.'''
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
