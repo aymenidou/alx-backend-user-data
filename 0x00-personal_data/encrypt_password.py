64a1cccac2d167417c9bd22237827a44603355cf
Aymen Idouahman <aymanidou@gmail.com>
2024-04-11 21:41:46 +0000

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,11 @@ def hash_password(password: str) -> bytes:
     '''function that expects one string argument name password and returns
       a salted, hashed password, which is a byte string.'''
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''function that expects 2 arguments and returns a boolean.
+        Arguments:
+            hashed_password: bytes type
+            password: string type'''
+    return bcrypt.checkpw(password.encode(), hashed_password)
