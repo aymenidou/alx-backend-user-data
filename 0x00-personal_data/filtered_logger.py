9ffe658a36b6f49e3eb2ee1ffe36573e79e5ce60
Aymen Idouahman <aymanidou@gmail.com>
2024-04-14 00:14:10 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''0x00-personal_data'''
+import logging
 from typing import List
 import re
 
@@ -17,3 +18,24 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         pattern = re.compile(fr'({param_name}=)[^{separator}]+')
         message = re.sub(pattern, fr'\g<1>{redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        '''initialisation'''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''format log record and hide sensitive info'''
+        message = super(RedactingFormatter, self).format(record)
+        exposed_message = filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
+        return exposed_message
