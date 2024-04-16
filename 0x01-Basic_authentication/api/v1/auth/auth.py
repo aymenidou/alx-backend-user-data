1f0b3b286c21d01ab62807c529185e405f1c71d7
Aymen Idouahman <aymanidou@gmail.com>
2024-04-16 23:51:43 +0100

    implement Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,9 +12,9 @@ class Auth():
         return False
 
     def authorization_header(self, request=None) -> str:
-        ''''''
+        '''authorization header'''
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        ''''''
+        '''current user'''
         return None
