cc17ba6a4c5ce88288e701d1a24a49acc3ee8113
Aymen Idouahman <aymanidou@gmail.com>
2024-04-16 23:50:25 +0100

    implement Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+'''authentification module'''
+
+
+class Auth():
+    '''Auth class'''
+    from flask import request
+    from typing import List, TypeVar
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''require auth'''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        ''''''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''''''
+        return None
