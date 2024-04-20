703cc3f2cf04bcc4bab78b9b6bad8f0d7d268dae
Aymen Idouahman <aymanidou@gmail.com>
2024-04-20 23:00:51 +0100

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,19 @@ class BasicAuth(Auth):
             return decoded_string
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        ''' returns the user email and password from
+          the Base64 decoded value.'''
+        if (decoded_base64_authorization_header is None
+                or not isinstance(decoded_base64_authorization_header, str)):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        user_email, user_password = decoded_base64_authorization_header.split(
+            ':', 1)
+        return user_email, user_password
