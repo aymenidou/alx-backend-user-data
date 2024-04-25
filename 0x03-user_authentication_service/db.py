e8511eb44450d1ad9de81eb6fcee150c4d3889dc
Aymen Idouahman <aymanidou@gmail.com>
2024-04-25 23:16:21 +0100

    1. create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,11 @@ class DB:
 
     def add_user(self, email, hashed_password) -> User:
         """add user"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self.__session.rollback()
+            user = None
         return user
