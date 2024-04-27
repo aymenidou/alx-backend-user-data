c72ee607bd9ca7f414a755a7de621167da204a40
Aymen Idouahman <aymanidou@gmail.com>
2024-04-28 00:02:37 +0100

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,5 +52,16 @@ def logout():
     return redirect('/')
 
 
+@app.route("/profile", methods=['GET'])
+def profile():
+    """fetch user email"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if (user):
+        return jsonify({"email": user.email})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
