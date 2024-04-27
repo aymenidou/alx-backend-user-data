66dd8b042e0dfe09765c4a6cea3edd585e995efa
Aymen Idouahman <aymanidou@gmail.com>
2024-04-27 19:24:57 +0100

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,6 +52,14 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """destroy user session"""
+        try:
+            self._db.find_user_by(user_id=user_id)
+            self._db.update_user(session_id=None)
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """hash the password"""
