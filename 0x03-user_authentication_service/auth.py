b1bdcdf12a9dc8b091ed82550430d9193d5ac798
Aymen Idouahman <aymanidou@gmail.com>
2024-04-27 14:02:23 +0100

    implement Auth.create_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,6 +33,16 @@ class Auth:
             return False
         return False
 
+    def create_session(self, email: str) -> str:
+        """create session for user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user_id=user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """hash the password"""
