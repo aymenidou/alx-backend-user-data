609bf9af03935d47b2cea80062e39424da8e5ef9
Aymen Idouahman <aymanidou@gmail.com>
2024-04-27 13:19:21 +0100

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,6 +22,16 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError('User {} already exists'.format(email))
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """validate credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(), user.hashed_password):
+                return True
+        except NoResultFound:
+            return False
+        return False
+
 
 def _hash_password(password: str) -> bytes:
     """hash the password"""
